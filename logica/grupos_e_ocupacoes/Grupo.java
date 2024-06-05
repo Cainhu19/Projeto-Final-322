37937c1aada28c60ee7dae78df2cd20ca45fe5ea
Henrique Silva <henriquermdsilva@gmail.com>
2024-06-05 10:22:45 -0300

    Início da implementação de Grupo e Ocupacao

--- logica/grupos_e_ocupacoes/Grupo.java
+++ logica/grupos_e_ocupacoes/Grupo.java
@@ -1,5 +1,34 @@
 package logica.grupos_e_ocupacoes;
 
+import logica.dados.*;
+
+// Representa os grupos (entidades, ligas, etc) que um jogador pode fazer parte
 public class Grupo {
+    private final String nome;
+    private final Dado dado;
+    private final int qtdDados;
+    private boolean ocupado;
     
+    public Grupo(String nome, Dado dado, int qtdDados) {
+    	this.nome = nome;
+    	this.dado = dado;
+    	this.qtdDados = qtdDados;
+    	this.ocupado = false;
+    }
+    
+    public String getNome() {
+    	return nome;
+    }
+    
+    public Dado getDado() {
+    	return dado;
+    }
+    
+    public int qtdDados() {
+    	return qtdDados;
+    }
+    
+    public boolean isOcupado() {
+    	return ocupado;
+    }
 }
--- logica/grupos_e_ocupacoes/Ocupacao.java
+++ logica/grupos_e_ocupacoes/Ocupacao.java
@@ -1,5 +1,26 @@
 package logica.grupos_e_ocupacoes;
 
+// Representa as ocupações que um jogador pode ter
 public class Ocupacao {
+    private final String nome;
+    private final int renda;
+    private boolean ocupada;
     
+    public Ocupacao(String nome, int renda) {
+    	this.nome = nome;
+    	this.renda = renda;
+    	this.ocupada = false;
+    }
+    
+    public String getNome() {
+    	return nome;
+    }
+    
+    public int getRenda() {
+    	return renda;
+    }
+    
+    public boolean isOcupada() {
+    	return ocupada;
+    }
 }
