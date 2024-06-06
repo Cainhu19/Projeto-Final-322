276895882a03af8e24908ffcb5fc7091fa232eb5
Cainhu19 <caioenzo92@outlook.com>
2024-06-06 14:49:35 -0300

    Rascunho das classes Jogo e Tabuleiro

--- logica/Jogo.java
+++ logica/Jogo.java
@@ -1,5 +1,26 @@
 package logica;
 
+import java.util.LinkedList;
+
+import logica.tabuleiro.Espaco;
+import logica.tabuleiro.Tabuleiro;
+
 public class Jogo {
-    
+    private static Jogo jogoInstance;
+    private LinkedList<Jogador> jogadores;
+    private Tabuleiro tabuleiro;
+
+    private Jogo(LinkedList<Jogador> jogadores, int nEspacos, LinkedList<Espaco> espacos) {
+        this.jogadores = jogadores;
+        this.tabuleiro = Tabuleiro.getInstance(nEspacos, espacos);
+    }
+
+    public static Jogo getInstance(LinkedList<Jogador> jogadores, int nEspacos, LinkedList<Espaco> espacos) {
+        if (jogoInstance == null) {
+            jogoInstance = new Jogo(jogadores, nEspacos, espacos);
+        }
+
+        return jogoInstance;
+    }
+
 }
--- logica/tabuleiro/Tabuleiro.java
+++ logica/tabuleiro/Tabuleiro.java
@@ -1,5 +1,22 @@
 package logica.tabuleiro;
 
+import java.util.LinkedList;
+
 public class Tabuleiro {
-    //TODO: singleton aqui
+    // TODO: singleton aqui
+    private static Tabuleiro tabuleiroInstance;
+    private LinkedList<Espaco> espacos;
+
+    private Tabuleiro(int n, LinkedList<Espaco> espacos) {
+        this.espacos = new LinkedList<Espaco>();
+        this.espacos = espacos;
+    }
+
+    public static Tabuleiro getInstance(int n, LinkedList<Espaco> espacos) {
+        if (tabuleiroInstance == null) {
+            tabuleiroInstance = new Tabuleiro(n, espacos);
+        }
+
+        return tabuleiroInstance;
+    }
 }
