59945bb7bff3cdca4bece39a55b27e846074fe10
Cainhu19 <caioenzo92@outlook.com>
2024-06-07 16:02:10 -0300

    Pequenos testes na main

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import logica.Jogo;
 import logica.tabuleiro.Caminho;
 import logica.tabuleiro.Espaco;
 import logica.tabuleiro.EspacoRemuneracao;
+import logica.tabuleiro.Tabuleiro;
 
 public class Main {
     public static void main(String args[]) {
@@ -21,11 +22,17 @@ public class Main {
         Espaco e1 = new EspacoRemuneracao("Inicio");
         Espaco e2 = new EspacoRemuneracao("Formatura do Médio");
         Espaco e3 = new EspacoRemuneracao("Preparações da Faculdade");
+        Espaco e4 = new EspacoRemuneracao("smt");
 
         c1.adicionarEspaco(e1);
         c1.adicionarEspaco(e2);
         c1.adicionarEspaco(e3);
+        c1.adicionarEspaco(e4);
+
+        Tabuleiro tabuleiro = Tabuleiro.getInstance();
+        tabuleiro.adicionarCaminho(c1);
 
         Jogo jogo = Jogo.getInstance(jogadores);
+        jogo.loopDeJogo();
     }
 }
--- logica/Jogo.java
+++ logica/Jogo.java
@@ -1,8 +1,8 @@
 package logica;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
-import logica.tabuleiro.Espaco;
 import logica.tabuleiro.Tabuleiro;
 
 public class Jogo {
@@ -27,7 +27,11 @@ public class Jogo {
 
         while (true) {
             for (Jogador jogador : jogadores) {
+                System.err.printf("Jogador %s joga o dado: %d\n", jogador.getNome(), jogador.resultadoDado(0));
+                Scanner sc = new Scanner(System.in);
 
+                System.err.println("Digite para sair");
+                sc.next();
             }
         }
     }
