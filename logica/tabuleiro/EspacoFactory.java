ba8ab55d05674ddac351dd0ea8b11624a018d6e3
GMT <giullianomteixeira@gmail.com>
2024-06-14 18:13:06 -0300

    Tabuleiro adicionado até caminho 9

--- logica/tabuleiro/EspacoFactory.java
+++ logica/tabuleiro/EspacoFactory.java
@@ -23,6 +23,8 @@ public class EspacoFactory {
                 FonteDeRenda fonteDeRenda = FonteDeRenda.valueOf(espacoElement.getElementsByTagName("fonteDeRenda").item(0).getTextContent());
                 return new EspacoOferta(desc, fonteDeRenda);
             // case "RECEBER_OFERTA_RENDA_GERAL":
+
+            // case "PERDER_RENDA":
                 
             case "RECEBER_OFERTA_GRUPO":
                 Grupo grupo = Grupo.valueOf(espacoElement.getElementsByTagName("grupo").item(0).getTextContent());
@@ -43,15 +45,41 @@ public class EspacoFactory {
 
             // case "DINHEIRO_SE_TIVER_RENDA_ESPECIFICA":
 
+            // case "DINHEIRO_POR_P_NETWORKING"
+
             // case "PAGAR_COM_OUTRO_JOGADOR":
 
-            // case "ESTUDO_PARA_IC"
+            // case "ESTUDO_PARA_IC":
 
-            // case "GANHAR_P_OPORTUNIDADES_SE_ESTIVER_EM_GRUPO"
+            // case "P_OPORTUNIDADES":
 
-            // case "PAGAR_PARA_P_NETWORKING"
+            // case "GANHAR_P_OPORTUNIDADES_SE_ESTIVER_EM_GRUPO":
 
-            // case "ROLAR_DADO_DE_NOVO"
+            // case "PERDER_GRUPO":
+
+            // case "PAGAR_PARA_P_NETWORKING":
+
+            // case "PAGAR_PARA_ESTUDO":
+
+            // case "ROLAR_DADO_DE_NOVO":
+
+            // case "RECEBER_OFERTA_EMPREGO_SE_P_NETWORKING_SUFICIENTE":
+
+            // case "CAMPEONATO_ATLETICA":
+
+            // case "ESPACO_49B":
+
+            // case "CASA_DO_AZAR":
+
+            // case "GANHAR_P_NETWORKING_COM_MAIOR_DADO":
+
+            // case "OPORTUNIDADE_DE_INTERCAMBIO":
+
+            // case "PROMOCAO_LOJA":
+
+            // case "LIMPAR_DIVIDA":
+
+            // case "ESCOLHER_JOGADOR_PARA_VOLTAR":
                 
             // case "EXTREMIDADE":
 
