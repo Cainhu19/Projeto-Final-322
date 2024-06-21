9c8267bb15f8e6234dcecbd15376ea176fc9761a
henriquermdsilva <henriquermdsilva@gmail.com>
2024-06-21 13:02:43 -0300

    Configuração Maven concluída

--- projetofinal322/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.projetofinal322 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-    
-
-    opens com.projetofinal322 to javafx.fxml;
-    exports com.projetofinal322;
-}
