bbc715294779a98e036b2bf7057f0feda9ff3e7f
Cainhu19 <caioenzo92@outlook.com>
2024-06-24 13:57:54 -0300

    merge e todo removido

--- src/main/java/com/projetofinal322/logica/Loja.java
+++ src/main/java/com/projetofinal322/logica/Loja.java
@@ -9,16 +9,17 @@ public class Loja {
     private static Dado[] dadosAVenda = new Dado[5];
     private static int[] precosDosDados;
 
-    // Inicializador estático para garantir que os dados e seus preços sejam configurados ao carregar a classe
+    // Inicializador estático para garantir que os dados e seus preços sejam
+    // configurados ao carregar a classe
     static {
         dadosAVenda = new Dado[] {
-            new D6(1, 1, 1, 1, 1, 1), // D6 com 6 faces de 1
-            new D6(6, 6, 6, 6, 6, 6), // D6 com 6 faces de 6
-            new D2(0, 1), // Moeda
-            new D8(0, 1, 1, 2, 3, 5, 8, 13), // Dado Fibonacci
-            new D4(2, 4, 6, 8) // D4 com faces 2, 4, 6 e 8
+                new D6(1, 1, 1, 1, 1, 1), // D6 com 6 faces de 1
+                new D6(6, 6, 6, 6, 6, 6), // D6 com 6 faces de 6
+                new D2(0, 1), // Moeda
+                new D8(0, 1, 1, 2, 3, 5, 8, 13), // Dado Fibonacci
+                new D4(2, 4, 6, 8) // D4 com faces 2, 4, 6 e 8
         };
-        precosDosDados = new int[] {1500, 3000, 1500, 2100, 2000};
+        precosDosDados = new int[] { 1500, 3000, 1500, 2100, 2000 };
     }
 
     public static Dado[] getDadosAVenda() {
@@ -30,14 +31,16 @@ public class Loja {
     }
 
     /**
-     * Avisa ao jogador j que ele não pode comprar um dado da loja -- porque ele já possui um dado comprado e precisa usá-lo antes.
+     * Avisa ao jogador j que ele não pode comprar um dado da loja -- porque ele já
+     * possui um dado comprado e precisa usá-lo antes.
      */
     private static void avisoVendaProibida(Jogador j) {
         System.out.println("Você já possui um dado comprado da loja e não pode ter outro.");
     }
 
     /**
-     * Entrega o dado comprado ao jogador que o comprou, o cobrando o preço apropriado. 
+     * Entrega o dado comprado ao jogador que o comprou, o cobrando o preço
+     * apropriado.
      */
     private static void posVenda(Jogador j, Dado d, int preco) {
         Dado[] novosDados = j.getDados();
@@ -71,9 +74,10 @@ public class Loja {
     }
 
     /**
-     * Realiza a ação da compra de um dado por um jogador j, ou a impede se necessário.
+     * Realiza a ação da compra de um dado por um jogador j, ou a impede se
+     * necessário.
      * 
-     * @param j jogador que está realizando a compra.
+     * @param j       jogador que está realizando a compra.
      * @param escolha o índice do dado escolhido nas arrays de dados e preços.
      */
     public static void compra(Jogador j, int escolha) {
@@ -128,12 +132,12 @@ public class Loja {
     public static void promocaoRelampago(Jogador j) {
         lojaEmPromocao();
         mostrarDadosDisponiveis();
-        System.out.println("Você deseja comprar algum dado? Se sim, digite o número do dado desejado; se não, digite 0.");
+        System.out
+                .println("Você deseja comprar algum dado? Se sim, digite o número do dado desejado; se não, digite 0.");
         System.out.println("Atenção: a promoção acaba no fim de sua rodada!");
         int escolha = Entrada.respostaInt();
         if (escolha != 0) {
-            compra(j, escolha); //TODO: isso aqui pressupõe entrada bem formatada, na GUI pode haver um jeito melhor
-
+            compra(j, escolha);
         }
         System.out.println("Fim da promoção relâmpago da Lojinha do Destino!");
         fimDaPromocao();
